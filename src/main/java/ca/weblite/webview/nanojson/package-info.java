da4ff3894d086b550717a57acbbbace83758d125
Will Iverson <wiverson@gmail.com>
2021-07-02 18:03:19 -0700

    Move to mvn dependency

--- src/main/java/ca/weblite/webview/nanojson/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Lightweight JSON input/output library. 
- * 
- * For reading JSON, see {@link com.grack.nanojson.JsonParser}. For writing JSON, see 
- * {@link com.grack.nanojson.JsonWriter}.
- */
-package ca.weblite.webview.nanojson;
-
